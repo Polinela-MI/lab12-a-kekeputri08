4376f2deb217495780de9813c82e1e801212df96
kekeputri08 <113098935+kekeputri08@users.noreply.github.com>
2022-11-24 15:33:15 +0700

    Update Latihan.java
    sop

--- src/Latihan.java
+++ src/Latihan.java
@@ -11,10 +11,18 @@ public class Latihan {
     public static void calculator(){
         File input = new File("docs/newfile.txt");
         File output = new File("docs/out.txt");
+        File csv = new File("docs/output.csv");
+
 
         try {
             Scanner scanner = new Scanner(input);
             PrintWriter writer = new PrintWriter(output);
+
+            System.out.println("Pilih tipe file output yang diinginkan :");
+            System.out.println("1. Text File");
+            System.out.println("2. CSV File");
+            System.out.println("-------------------");
+            System.out.println("Masukkan Pilihan : ");
 
             while (scanner.hasNextInt()){
                 String[] hasilSplit = scanner.nextLine().split(" ");
